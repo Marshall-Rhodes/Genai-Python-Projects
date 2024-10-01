01f1e2d59dc7c775743197a008e06bd1060a8028
Marshall-Rhodes <156715267+Marshall-Rhodes@users.noreply.github.com>
2024-10-01 13:21:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,76 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain import hub
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain_community.vectorstores import Chroma
-from dotenv import load_dotenv, dotenv_values
+import streamlit as st
+import pandas as pd
+import langchain
 import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from pandasai import SmartDataframe
+from langchain.agents import create_openapi_agent
 
-#Setting up paths and OpenAI Chatbot Model
-DATA_PATH = r"/Users/MarshallRhodes/Documents/Python/RAG Chatbot/PDFs"
-CHROMA_PATH = "chroma"
-load_dotenv()
+#loading OpenAI API key
+load_dotenv(dotenv_path=r"C:\Users\mrhodes\OneDrive - Deloitte (O365D)\Documents\Python\PersonalProjects\Streamlit_excel_tool\OPEN_AI_API_KEY.env")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-model = ChatOpenAI(api_key = OPENAI_API_KEY)
+os.environ["PANDASAI_API_KEY"] = OPENAI_API_KEY
 
-#Loading PDFs folder in Langchain PDF directory loader
-loader = PyPDFDirectoryLoader(DATA_PATH)
-documents = loader.load()
+def create_agent(df):
+    model = ChatOpenAI(temperature=0.7, api_key=OPENAI_API_KEY)
+    agent = SmartDataframe(df=df, config={"llm":model})
+    return agent
 
-#Splitting documents
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 500, add_start_index = True)
-chunks = text_splitter.split_documents(documents)
+def query_agent(query, agent):
+    question = """
+    You are a helpful agent that will help the user interact with a dataset. Please respond only in JSON format.
+    Please answer the query down below:
 
-#Building a vectorstore
-vectorestore = Chroma.from_documents(documents = chunks, embedding=OpenAIEmbeddings(api_key=OPENAI_API_KEY))
-retriever = vectorestore.as_retriever()
+    """ + query
+    response = agent.chat(question)
+    return response
 
-#Setting up prompt and ragchain
-prompt = hub.pull("rlm/rag-prompt", api_key=OPENAI_API_KEY)
 
-def format_docs(docs):
-    return "/n/n".join(doc.page_content for doc in docs)
+st.title("Marshall's Magic GenAI Machine (Beta)")
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
+#Sidebar - where file will be uploaded and possibly so additional documentation
+#additional features - need to accept more than just .csv
+with st.sidebar:
+    uploaded_file = st.file_uploader('Please enter .csv file here and watch the magic happen.')
 
-while True:
-    query = input("What would you like to know from your PDFs?")
-    if query == "q":
-        break
-    else:
-        answer = rag_chain.invoke(query)
-        print(answer)
+#Chat and dataframe will only appear once the file has been uploaded
+#Loading data to dataframe and presenting to user
+if uploaded_file is not None:
+    with st.expander("Your Data:"):
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(df.head())
+        agent = create_agent(df)
+
+    with st.form("my_form"):
+        text = st.text_area("What would you like to ask the magic AI Assistant:")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            response = query_agent(query=text, agent=agent)
+            st.info(response)
+
+    #Building the chat function
+    #if "openai_model" not in st.session_state:
+        #st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    #Initialize chat history
+    #if "messages" not in st.session_state:
+        #st.session_state.messages = []
+    
+    #Display chat messages from history on app rerun
+    #for message in st.session_state.messages:
+        #with st.chat_message(message["role"]):
+            #st.markdown(message["content"])
+
+    #if prompt := st.chat_input("What would you like to ask the magic AI Assistant?"):
+        #with st.chat_message("user"):
+            #st.markdown(prompt)
+        
+        #st.session_state.messages.append({"role": "user", "content": prompt})
+
+        #response = f"User message: {prompt}"
+        #with st.chat_message("Magic AI Assitant"):
+            #st.markdown(response)
+
+        #st.session_state.messages.append({"role": "assistant", "content": response})
